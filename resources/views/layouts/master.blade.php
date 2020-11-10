9c4748749acfd02bd14501f6caa3fc670e4faebb
unknown <antonhilman103@gmail.com>
2020-11-10 15:46:06 +0700

    [W] Static Page

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+  @include('layouts.head')
+</head>
+  <div class="preloader">
+        <svg class="circular" viewBox="25 25 50 50">
+            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>
+    </div>
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper">
+
+  <header class="topbar">
+    @include('layouts.header')
+  </header>
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="left-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    @include('layouts.sidebar')
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="page-wrapper">
+    <!-- Content Header (Page header) -->
+    
+
+    <!-- Main content -->
+
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+             <footer class="footer">
+                © 2019 Material Pro Admin by wrappixel.com
+            </footer>
+
+  <!-- Control Sidebar -->
+  
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+
+<!-- ./wrapper -->
+
+@include('layouts.script')
+
+</body>
+</html>
---
+++ resources/views/layouts/script.blade.php
@@ -0,0 +1,33 @@
+ <script data-cfasync="false" src="{{URL::to('cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js')}}"></script>
+ <script src="{{URL::to('assets/plugins/jquery/jquery.min.js')}}"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="{{URL::to('assets/plugins/popper/popper.min.js')}}"></script>
+    <script src="{{URL::to('assets/plugins/bootstrap/assets/js/bootstrap.min.js')}}"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="{{URL::to('assets/js/jquery.slimscroll.js')}}"></script>
+    
+    <!--Wave Effects -->
+    <script src="{{URL::to('assets/js/waves.js')}}"></script>
+    <!--Menu sidebar -->
+    <script src="{{URL::to('assets/js/sidebarmenu.js')}}"></script>
+    <!--stickey kit -->
+    <script src="{{URL::to('assets/plugins/sticky-kit-master/dist/sticky-kit.min.js')}}"></script>
+    <script src="{{URL::to('assets/plugins/sparkline/jquery.sparkline.min.js')}}"></script>
+    <script src="{{URL::to('assets/plugins/sparkline/jquery.sparkline.min.js')}}"></script>
+    <!--Custom JavaScript -->
+    <script src="{{URL::to('assets/js/custom.min.js')}}"></script>
+    <!-- ============================================================== -->
+    <!-- This page plugins -->
+    <!-- ============================================================== -->
+    <!-- chartist chart -->
+    <script src="{{URL::to('assets/plugins/chartist-assets/js/dist/chartist.min.js')}}"></script>
+    <script src="{{URL::to('assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.min.js')}}"></script>
+    <!--c3 JavaScript -->
+    <script src="{{URL::to('assets/plugins/d3/d3.min.js')}}"></script>
+    <script src="{{URL::to('assets/plugins/c3-master/c3.min.js')}}"></script>
+    <!-- Chart JS -->
+    <script src="{{URL::to('assets/js/dashboard1.js')}}"></script>
+    <!-- ============================================================== -->
+    <!-- Style switcher -->
+    <!-- ============================================================== -->
+    <script src="{{URL::to('assets/plugins/styleswitcher/jQuery.style.switcher.js')}}"></script>
