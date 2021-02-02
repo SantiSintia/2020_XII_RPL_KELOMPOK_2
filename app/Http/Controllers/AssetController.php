0b83ec20c55f8adb6d07bedb6a1cabd093cccb25
gararetek44 <taupikpathurrohman@gmail.com>
2021-02-02 23:38:09 +0700

    [f-12]detail asset

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -85,9 +85,10 @@ class AssetController extends Controller
         $asset = Asset::join('asset_categories','ass_asset_category_id','=','asc_id')
                       ->join('asset_types','ass_asset_type_id','=','ast_id')
                       ->join('origins','ass_origin_id','=','ori_id')
+                      ->join('asset_descriptions','ass_id','=','asd_ass_id')
                       ->whereAssId($id)->first();
-        dd($asset);
-        return view('assets.list-detail-asset', compact('asset'));
+        // dd($asset);
+        return view('assets.list-detail-asset', compact(['asset']));
     }
 
     /**
